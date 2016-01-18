d54366c7bce586c8aa249207d02c3e59b16bd188
db2 <gylimingqi@163.com>
2016-01-18 11:57:16 +0800

    add var.py

--- str_format.py
+++ str_format.py
@@ -4,3 +4,7 @@ age=25
 name='Swaroop'
 print('{0} is {1} years old'.format(name, age)) 
 print('Why is {0} playing with that python?'.format(name))
+a='{0} wrote {1}'.format('Swaroop', 'A Byte of Python')
+print(a)
+b='{0:_^11}'.format('hello')
+print(b)
