6b145789342276cfc8ee2c9fb98311ebbfda1543
cglmq-vm <gylimingqi@163.com>
2016-01-22 19:56:56 +0800

    add break.py continue.py for.py function1.py

---
+++ for.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#Filename: for.py
+
+for i in range (1,5):
+	print (i)
+else:
+	print ('The for loop is over')
+
---
+++ function1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Filename: function1.py
+def sayHello():
+	print('Hello World')#block belonging to the function
+#End of function
+sayHello()#callthe function
+sayHello()#call the function again
