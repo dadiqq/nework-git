4190ca764770e46ddf374a0abeee32733c9e05db
db2 <gylimingqi@163.com>
2016-01-23 00:26:25 +0800

    add allfiles

---
+++ ersioncheck.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Filename: versioncheck.py
+
+import sys, warnings
+
+if sys.version_info[0] < 3:
+	warnings.warn("Need Python 3.0 for this program to run",
+		RuntimeWarning)
+else:
+	print('Proceed as normal')
