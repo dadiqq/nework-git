c3af4b8384c634820fcd618850e4e3381d13f8b2
db2 <gylimingqi@163.com>
2016-01-23 13:43:39 +0800

    2016-01-23 13:43:39  M  gitadd.py M  gitadd.txt

--- gitadd.py
+++ gitadd.py
@@ -1,13 +1,13 @@
 
-git status -s echo "one"
+git status -s 
 
 git status -s  | awk ' $2 { print ( $2 ) }'>gitadd.txt
 
 
 git add `more  gitadd.txt`
 
-git status -s echo"two"
+git status -s 
 
 git commit -m " `date "+%Y-%m-%d %H:%M:%S"`  `git status -s`"
 
-git status -s  echo "end"
+git status -s  
