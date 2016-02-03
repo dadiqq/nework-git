ac7aadeeb50c3b45a77f065af18f69735cbbd31e
limq-debianvm-lmq <gylimingqi@163.com>
2016-02-03 19:42:02 +0800

    2016-02-03 19:42:02  M  gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -2,3 +2,10 @@ git push -u origin master
 #git remote rm origin
 #git remote -v #--查看远程库
 #git remote -v #--查看远程库#remote add origin  git@github.com:dadiqq/nework-git.git#添加远程库
+#Git鼓励大量使用分支：
+#查看分支：git branch
+#创建分支：git branch <name>
+#切换分支：git checkout <name>
+#创建+切换分支：git checkout -b <name>
+#合并某分支到当前分支：git merge <name>
+#删除分支：git branch -d <name>
