235a605019202c724ab85899498f23b48b9c2a0d
limq <limq@debian>
2016-02-03 19:35:40 +0800

    2016-02-03 19:35:40  M  gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -1,1 +1,5 @@
 git clone  git@github.com:dadiqq/nework-git.git
+##git checkout -b dev  --创建并切换到 dev分支    git branch  ---查看分支
+##git checkout -b dev  --创建并切换到 dev分支  ##  git branch  ---查看分支
+#git checkout master --切换分支
+#git merge dev  --合并到当前分支
