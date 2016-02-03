1a111982bb61a1aa147f2474888437e91f019fda
limq-debianvm-lmq <gylimingqi@163.com>
2016-02-03 19:37:21 +0800

    2016-02-03 19:37:21  M  gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -3,3 +3,4 @@ git clone  git@github.com:dadiqq/nework-git.git
 ##git checkout -b dev  --创建并切换到 dev分支  ##  git branch  ---查看分支
 #git checkout master --切换分支
 #git merge dev  --合并到当前分支
+#git branch -d dev   --删除分支
