ccca68baf803e5ac8c4511dff98654fc9143b7a7
debian <gylimingqi@163.com>
2017-02-12 21:01:39 +0800

    2017-02-12 21:01:39  M  git-config.sh

--- git-config.sh
+++ git-config.sh
@@ -31,3 +31,22 @@ git clone  git@github.com:dadiqq/nework-git.git
 #git checkout master --切换分支
 #git merge dev  --合并到当前分支
 #git branch -d dev   --删除分支
+git push -u origin master
+#git remote rm origin
+#git remote -v #--查看远程库
+#git remote -v #--查看远程库#remote add origin  git@github.com:dadiqq/nework-git.git#添加远程库
+#Git鼓励大量使用分支：
+#查看分支：git branch
+#创建分支：git branch <name>
+#切换分支：git checkout <name>
+#创建+切换分支：git checkout -b <name>
+#合并某分支到当前分支：git merge <name>
+#删除分支：git branch -d <name>
+#用带参数的git log也可以看到分支的合并情况：
+
+# git log --graph --pretty=oneline --abbrev-commit
+#小结
+
+#当Git无法自动合并分支时，就必须首先解决冲突。解决冲突后，再提交，合并完成。
+
+#用git log --graph命令可以看到分支合并图。
